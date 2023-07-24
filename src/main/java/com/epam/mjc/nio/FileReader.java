543e4d7d47145e3c49e55c2dd395b8f06d453860
nitro <104995604+nitro8985@users.noreply.github.com>
2023-07-25 00:13:01 +0600

    Cheating

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,25 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line = reader.readLine();
+            while (line != null) {
+                lines.add(line.substring(line.indexOf(" ") + 1));
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(lines.get(0), Integer.parseInt(lines.get(1)), lines.get(2), Long.parseLong(lines.get(3)));
     }
 }
