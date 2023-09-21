8091c0cc16fe94b4651ab8af87a0e21ce1054dde
nitro <104995604+nitro8985@users.noreply.github.com>
2023-09-21 20:50:09 +0600

    Done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,8 +1,8 @@
 package com.epam.mjc.nio;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,16 +10,32 @@ import java.util.List;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
-            String line = reader.readLine();
-            while (line != null) {
-                lines.add(line.substring(line.indexOf(" ") + 1));
-                line = reader.readLine();
-            }
+        List<String> lines = new ArrayList<>(4);
+        try (RandomAccessFile accessFile = new RandomAccessFile(file.getAbsolutePath(), "r");
+             FileChannel channel = accessFile.getChannel()) {
+            ByteBuffer buffer = ByteBuffer.allocate(512);
+            channel.read(buffer);
+            buffer.flip();
+            lines = separateIntoLines(buffer);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new Profile(lines.get(0), Integer.parseInt(lines.get(1)), lines.get(2), Long.parseLong(lines.get(3)));
+        return new Profile(lines.get(0), Integer.valueOf(lines.get(1)), lines.get(2), Long.parseLong(lines.get(3)));
+    }
+
+    private List<String> separateIntoLines(ByteBuffer buffer) {
+        List<String> lines = new ArrayList<>();
+        while (buffer.hasRemaining()) {
+            StringBuilder line = new StringBuilder();
+            for (int j = 0; j < buffer.limit(); j++) {
+                char c = (char) buffer.get();
+                if (c == '\n') {
+                    break;
+                }
+                line.append(c);
+            }
+            lines.add(line.substring(line.indexOf(" ") + 1).trim());
+        }
+        return lines;
     }
 }
